0b9e42cff50c948aec90edb7fcda0a201ba19b82
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 03:23:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+def add_name_to_node(name)
+  hash = {
+    name => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+  }
+  hash
+end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+
 end
