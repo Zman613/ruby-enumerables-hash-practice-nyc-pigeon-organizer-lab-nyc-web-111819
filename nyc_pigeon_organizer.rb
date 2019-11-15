7a0c22aee0caf1b8b0e0475c1e24f65ec438c1c6
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 05:13:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -32,7 +32,7 @@ def nyc_pigeon_organizer(data)
         if pigeon_list[name] == nil
           pigeon_list[name][cgl] = []
         end
-        pigeon_list[name][cgl] = info
+        pigeon_list[name][cgl].push(info.to_s)
       end
     end
   end
