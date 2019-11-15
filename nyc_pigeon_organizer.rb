ef060b0ec583ba5e0b1c0f586b962610de4a354c
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 05:29:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
+def turn_keys_into_strings(hash)
+  hash.transform_keys do |key|
+    key.to_s
+  end
+end
+
+def add_name_to_node(name)
+  hash = {
+    name => {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+  }
+  hash
+end
+
+def array_of_array(hash)
+  array = []
+  hash.reduce(nil) do |memo, (key, value)|
+    array << value
+  end
+  array
+end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |cgl, value|
+    value.each do |info, names|
+      names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][cgl] == nil
+          pigeon_list[name][cgl] = []
+        end
+        pigeon_list[name][cgl].push(info.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
