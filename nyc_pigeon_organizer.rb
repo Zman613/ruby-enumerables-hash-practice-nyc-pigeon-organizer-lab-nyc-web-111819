4be94091dfd1a8d177560d1d1b6276ce7e2e562d
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 04:58:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,8 +29,8 @@ def nyc_pigeon_organizer(data)
   data.each do |cgl, value|
     value.each do |info, names|
       names.each do |name|
-        if !pigeon_list[name]
-        pigeon_list[name] = {}
+        if pigeon_list[name] == nil
+        pigeon_list << add_name_to_node(name)
         end
         pigeon_list[name][cgl] = [info].to_s
       end
